709fb4bdf69ea6132b0ccbbc3134b8436707b788
xolmusic <xol@xolmusic.com>
2026-07-13 03:48:22 +0000

    Update Cover.tsx

--- src/components/Cover.tsx
+++ src/components/Cover.tsx
@@ -1,5 +1,5 @@
 // Pochette d'un titre ou projet. Si aucun visuel n'est fourni, on affiche
-// le logo XOL carre par defaut (au lieu d'un carre gris vide).
+// le logo XOL carre par defaut sur fond blanc.
 export default function Cover({
   src,
   size,
@@ -9,33 +9,31 @@ export default function Cover({
   size: number | string;
   radius?: number;
 }) {
-  const box: React.CSSProperties = {
-    width: size,
-    height: size,
-    borderRadius: radius,
-    flexShrink: 0,
-    overflow: "hidden",
-    background: "#fff",
-    border: "1px solid var(--border)",
-  };
-
-  if (src) {
-    return (
-      <div style={box}>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src={src} alt="" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
-      </div>
-    );
-  }
-
-  // Repli : logo XOL centre sur fond blanc.
   return (
-    <div style={{ ...box, display: "grid", placeItems: "center", padding: "14%" }}>
+    <div
+      style={{
+        width: size,
+        height: size,
+        borderRadius: radius,
+        flexShrink: 0,
+        overflow: "hidden",
+        background: "#ffffff",
+        border: "1px solid var(--border)",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
-        src="/brand/xol-square.jpg"
-        alt="XOL Music"
-        style={{ width: "100%", height: "100%", objectFit: "contain" }}
+        src={src || "/brand/xol-square.jpg"}
+        alt=""
+        style={{
+          width: "100%",
+          height: "100%",
+          objectFit: src ? "cover" : "contain",
+          padding: src ? 0 : "12%",
+        }}
       />
     </div>
   );
